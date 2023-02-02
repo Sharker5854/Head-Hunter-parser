c2234f1c7752d4ca78d8c5bfc67291a0a3ba890c
Vladimir <kiryanov_vova_134@mail.ru>
2023-02-02 22:10:17 +0300

    Refactored practically all code structure, corrected parsing from hh.ru, and implemented possibility of parsing from superjob.ru .

--- config.py
+++ config.py
@@ -1,7 +1,30 @@
-HOST = "https://hh.ru/"
-URL = "https://hh.ru/search/vacancy/"
+SITES = {
+	"HeadHunter": {
+		"host" : "https://hh.ru",
+		"url" : "https://hh.ru/search/vacancy/",
+		"area_param_name" : "area",
+		"profession_param_name" : "text",
+		"page_param_name" : "page"
+	},
+	"SuperJob": {
+		"host" : "https://www.superjob.ru",
+		"url" : "https://www.superjob.ru/vakansii/",
+		"area_param_name" : "geo[t][0]",
+		"profession_param_name" : "keywords",
+		"page_param_name" : "page"
+	},
+	"RabotaRu": {
+		"host" : "https://www.rabota.ru",
+		"url" : ""
+	},
+	"JobLab": {
+		"host" : "https://joblab.ru",
+		"url" : ""
+	}
+}
 
 HEADHUNTER_AREA_PARAM = 1  # номер обозначает город, в котором ищем работу (1 - Москва, 2 - Питер и т.д.)
+SUPERJOB_AREA_PARAM = 4  # 4 - Москва
 
 HEADERS = {
 	"accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
---
+++ parsers.py
@@ -0,0 +1,147 @@
+import csv
+import requests
+from requests import Response
+from bs4 import BeautifulSoup
+import config
+
+
+class Parser:
+	def __init__(self, profession, pages=3):
+		self.profession = profession
+		self.pages = pages
+		self.area_param_name = config.SITES[self.site]["area_param_name"]
+		self.profession_param_name = config.SITES[self.site]["profession_param_name"]
+		self.page_param_name = config.SITES[self.site]["page_param_name"]
+
+	def get_response(self, page) -> Response:
+		response = requests.get(
+			config.SITES[self.site]["url"], headers=config.HEADERS,
+			params={self.area_param_name: self.area, self.profession_param_name: self.profession, self.page_param_name: page},
+		)
+		return response
+
+	def get_page_content(self, response: Response):
+		"Вытащить все нужные элементы из полученного HTML-ответа"
+		raise NotImplementedError()
+
+
+class HeadHunter(Parser):
+	area = config.HEADHUNTER_AREA_PARAM
+	site = "HeadHunter"
+
+	def get_page_content(self, response):
+		'''Поиск всех данных на одной конкретной page'''
+		soup = BeautifulSoup(response.text, "html.parser")
+		vacancies = soup.find_all("div", class_="vacancy-serp-item__layout")
+		if len(vacancies) == 0:
+			return None
+		else:
+			content = []
+			for vac in vacancies:
+				title_block = vac.find("a", class_="serp-item__title")
+				content.append(
+					{
+						"title" : title_block.get_text(strip=True),
+						"link" : title_block.get("href").split("?")[0],
+						"salary" : vac.find("span", class_="bloko-header-section-3").get_text().replace("\u202f", "") if ( vac.find("span", class_="bloko-header-section-3") != None ) else "Не указано",
+						"company" : vac.find("a", class_="bloko-link bloko-link_kind-tertiary").get_text().replace("\xa0", " ") if ( vac.find("a", class_="bloko-link bloko-link_kind-tertiary") != None ) else "Компания не указана",
+					}
+				)
+			return content
+
+	def parse(self):
+		print(f"\nПоиск вакансий в Москве на {self.site}...\n")
+		responses = []
+		for page in range(self.pages):
+			responses.append(self.get_response(page))
+		data = []
+		if self.get_page_content(responses[0]) == None:
+			print("По вашему запросу ничего не найдено!")
+		else:
+			for resp in responses:
+				content = self.get_page_content(resp)
+				if content != None:
+					for dataset in content:
+						data.append(dataset)
+				else:
+					break
+		return data
+
+
+class SuperJob(Parser):
+	site = "SuperJob"
+	area = config.SUPERJOB_AREA_PARAM
+
+	def get_page_content(self, response):
+		'''Поиск всех данных на одной конкретной page'''
+		soup = BeautifulSoup(response.text, "html.parser")
+		vacancies = soup.find_all("div", class_="_1wWd1 _3irGY")
+		if len(vacancies) == 0:
+			return None
+		else:
+			content = []
+			for vac in vacancies:
+				title_block = vac.find("a", class_="YrERR")
+				salary_block = vac.find("div", class_="f-test-text-company-item-salary")
+				content.append(
+					{
+						"title" : title_block.get_text(strip=True),
+						"link" : config.SITES[self.site]["host"] + title_block.get("href"),
+						"salary" : salary_block.find("span", class_=["_3xCPT", "rygxv", "_17lam", "_3GtUQ"]).get_text().replace("&nbsp;", " ").replace("\u20bd", "руб") if ( salary_block.find("span", class_=["_3xCPT", "rygxv", "_17lam", "_3GtUQ"]).get_text().replace("&nbsp;", " ").replace("\u20bd", "руб") != None ) else "Не указано",
+						"company" : vac.find("a", class_="_198Ox").get_text() if ( vac.find("a", class_="_198Ox") != None ) else "Компания не указана",
+					}
+				)
+			return content
+
+	def parse(self):
+		print(f"\nПоиск вакансий в Москве на {self.site}...\n")
+		responses = []
+		for page in range(self.pages):
+			responses.append(self.get_response(page))
+		data = []
+		if self.get_page_content(responses[0]) == None:
+			print("По вашему запросу ничего не найдено!")
+		else:
+			for resp in responses:
+				content = self.get_page_content(resp)
+				if content != None:
+					for dataset in content:
+						data.append(dataset)
+				else:
+					break
+		return data
+
+
+def write_in_file(profession, dataset):
+	try:
+		with open(f"C:\\Users\\Admin\\Desktop\\{profession.title()}.{config.SAVE_FILE_EXTENSION}", "w", newline="") as file:
+			writer = csv.writer(file, delimiter=";")
+			for data in dataset:
+				writer.writerow([data[0]])
+				writer.writerow([""])
+				writer.writerow(["ДОЛЖНОСТЬ", "ЗАРПЛАТА", "КОМПАНИЯ", "ССЫЛКА НА ВАКАНСИЮ"])
+				writer.writerow([""])
+				for dataset in data[1]:
+					writer.writerow([ dataset["title"], dataset["salary"], dataset["company"], dataset["link"] ])
+				writer.writerow([""])
+				writer.writerow([""])
+				writer.writerow([""])
+		print(f"Файл {profession.title()}.{config.SAVE_FILE_EXTENSION} сохранён на рабочий стол!")
+	except:
+		print("Ошибка при записи в файл.")
+	input("\n\n-------------------------------\nНажмите Enter чтобы завершить.")
+
+
+def main():
+	profession = input("\nВведите интересующую профессию: ")
+	pages = int(input("Введите количество страниц для парсинга на каждом сайте: "))
+	site_results = [
+		(HeadHunter.site, HeadHunter(profession, pages).parse() ),
+		(SuperJob.site, SuperJob(profession, pages).parse() )
+	]
+	write_in_file(profession, site_results)
+	
+	
+
+if __name__ == "__main__":
+	main()
